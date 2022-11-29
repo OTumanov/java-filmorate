54f09c082e49a623c04bc04127d0d16e5090f933
otumanov <otumanov@gmail.com>
2022-11-29 21:30:40 +0300

    ТЗ 9 - v0.3

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    private int id;
+
+    @NotBlank
+    @NotNull
+    private String name;
+
+    @Size(max = 200)
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private int duration;
+
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+}
