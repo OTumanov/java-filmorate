c838e961e068fbba4c571383d8b5de3808eda574
olegitumanov <otumanov@gmail.com>
2023-04-23 15:51:23 +0300

    - финишный вариант

--- src/main/java/ru/yandex/practicum/filmorate/exception/MpaNotFoundExceprion.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class MpaNotFoundExceprion extends InvalidIdException{
-    public MpaNotFoundExceprion(Integer id) {
-        super("MPA c id " + id + " не найдено!");
-    }
-}
